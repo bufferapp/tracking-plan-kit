6066e98b693f1faa7d383aab61328849daa2329a
Michael Erasmus <hi@michaelerasm.us>
2019-07-10 16:02:01 -0700

    Cut new package version 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    version="0.0.1",
+    version="0.0.2",
     name="tracking-plan-kit",
     packages=["tracking_plan", "cli", "segment"],
     include_package_data=True,
