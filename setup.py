0d4fa7c33ab0f15279080d4c05eb2e9ec944a785
Michael Erasmus <hi@michaelerasm.us>
2019-08-02 12:01:26 -0700

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    version="0.0.4",
+    version="0.0.5",
     name="tracking-plan-kit",
     packages=["tracking_plan", "cli", "segment"],
     include_package_data=True,
